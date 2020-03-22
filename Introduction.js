8ede6d3395cdbf1d173f6fd185be8a9b825faa3b
Steveen <steveen.echeverri@gmail.com>
2020-03-22 12:39:06 -0500

    Excercises React Components basics

--- Introduction.js
+++ Introduction.js
@@ -72,3 +72,67 @@ const JSX = (
     <h1>Add a class to this div</h1>
   </div>
 );
+
+// Las etiquetas JSX de cierre automático
+
+// Cualquier elemento JSX puede escribirse con una etiqueta de cierre automático
+
+// <br />para que sea JSX válido que se pueda transpilar.
+//  se puede escribir como <div />o <div></div>. 
+
+const JSX = (
+  <div>
+    {/* remove comment and change code below this line
+    <h2>Welcome to React!</h2> <br >
+    <p>Be sure to close all tags!</p>
+    <hr >
+    remove comment and change code above this line */}
+    <br />
+    <hr />
+  </div>
+);
+
+// crear un componente funcional sin estado
+
+// Para crear un componente con una función, simplemente escriba una función de JavaScript que devuelva JSX o null.
+
+// After being transpiled, the <div> will have a CSS class of 'customClass'
+const DemoComponent = function() {
+  return (
+    <div className='customClass' />
+  );
+};
+
+// crear un componente de React
+
+// La otra forma de definir un componente React es con la classsintaxis ES6 
+
+class Kitten extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hi</h1>
+    );
+  }
+}
+
+// Example
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // change code below this line
+    return(
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    );
+
+  }
+};
+
