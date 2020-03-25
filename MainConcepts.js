1268cb076eb078acb5867ec8c82092ddcaa4858e
Steveen <steveen.echeverri@gmail.com>
2020-03-25 16:55:09 -0500

    Compose React Components

--- MainConcepts.js
+++ MainConcepts.js
@@ -87,3 +87,61 @@ const ChildComponent = () => {
     }
   };
   
+// Compose React Components
+  class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        { /* change code below this line */ }
+          <NonCitrus />
+          <Citrus />
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+     super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        { /* change code below this line */ }
+        <Fruits />
+        { /* change code above this line */ }
+        <Vegetables />
+      </div>
+    );
+  }
+};
+
+// renderizar un componente de clase al DOM
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* change code below this line */}
+          <Fruits />
+          <Vegetables />
+        {/* change code above this line */}
+      </div>
+    );
+  }
+};
+
+// change code below this line
+
+ReactDOM.render(<TypesOfFood />,
+document.getElementById('challenge-node'));
