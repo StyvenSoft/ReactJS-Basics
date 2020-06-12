08f104fef20ed35c3a3df0d53662368b7ae0f9a5
Steveen <steveen.echeverri@gmail.com>
2020-06-12 16:55:18 -0500

    Set the Input's Initial State

--- Lesson11/Input.js
+++ Lesson11/Input.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 
 export class Input extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = { userInput: '' };
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
   handleUserInput(e) {
     this.setState({
         userInput: e.target.value
